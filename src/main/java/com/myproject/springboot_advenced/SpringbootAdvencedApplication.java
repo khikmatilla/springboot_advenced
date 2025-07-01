ef2f3024b05b3db36d93e657fb82801a10dbc667
hikmatilla <hikmatillahabibullayev1999@gmail.com>
2025-07-01 20:18:25 +0500

    initial commit

---
+++ src/main/java/com/myproject/springboot_advenced/SpringbootAdvencedApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springboot_advenced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdvencedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdvencedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/springboot_advenced/SpringbootAdvencedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springboot_advenced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdvencedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
