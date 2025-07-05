4f4ed452dc0f3d1afb54c62efd098da515aea0b4
hikmatilla <xabibullayev.xikmatilla@zaytuntech.uz>
2025-07-05 18:02:08 +0500

    Lesson3

--- src/main/java/com/myproject/springboot_advenced/SpringbootAdvencedApplication.java
+++ src/main/java/com/myproject/springboot_advenced/SpringbootAdvencedApplication.java
@@ -2,22 +2,13 @@ package com.myproject.springboot_advenced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.time.Duration;
-
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootAdvencedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAdvencedApplication.class, args);
-    }
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.create();
     }
 }
--- src/main/java/com/myproject/springboot_advenced/service/PostServiceImpl.java
+++ src/main/java/com/myproject/springboot_advenced/service/PostServiceImpl.java
@@ -6,31 +6,23 @@ import com.myproject.springboot_advenced.dto.PostCreateDto;
 import com.myproject.springboot_advenced.dto.PostDto;
 import com.myproject.springboot_advenced.entity.Post;
 import com.myproject.springboot_advenced.repository.PostRepository;
-import com.myproject.springboot_advenced.resourc.CommentResources;
+import com.myproject.springboot_advenced.resourc.CommentClient;
 import lombok.NonNull;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Service
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
-    private final CommentResources commentResources;
+    private final CommentClient commentClientResources;
 
-    public PostServiceImpl(PostRepository postRepository, CommentResources commentResources) {
-        this.postRepository = postRepository;
-        this.commentResources = commentResources;
-    }
 
     @Override
     public PostDto getPost(@NonNull Integer id) {
         Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("Post not found with id !!"));
-        List<CommentDTO> comments = commentResources.getComments(id);
+        List<CommentDTO> comments = commentClientResources.getAllComments(id);
         return PostDto.builder()
                 .id(post.getId())
                 .title(post.getTitle())
@@ -55,6 +47,6 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void saveAllComments(@NonNull List<CommentCreateDTO> dtos) {
-       commentResources.saveAllComments(dtos);
+       commentClientResources.saveAllComments(dtos);
     }
 }
