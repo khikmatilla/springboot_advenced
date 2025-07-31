e1854360322ab27d7eb25851535ba862dc2a2a36
hikmatilla <xabibullayev.xikmatilla@zaytuntech.uz>
2025-07-31 12:22:53 +0500

    spring with webSocket

---
+++ src/main/java/com/myproject/springboot_advenced/dto/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.myproject.springboot_advenced.dto;
+
+import java.awt.*;
+
+public class ChatMessage {
+    private String sender;
+    private String recipient;
+    private String content;
+    private TrayIcon.MessageType type;
+
+    // getter-setter
+}
